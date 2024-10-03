8757cb0f4889d2dfed4f4e64e5da5cf032569edc
lchamprobert <lchamprobert@N112-04>
2024-10-03 15:44:11 +0200

    Ajoutez des fichiers projet.

---
+++ BoucleTestConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoucleTestConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int i;
+            Console.WriteLine("Nombres pairs entre 1 et 10 :");
+            for ( i = 0; i <= 10; i++)
+           {
+                if (i % 2 == 0)
+                    Console.WriteLine(i);
+           }
+             Console.ReadKey();
+
+            
+        }
+    }
+}
